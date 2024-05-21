f1160efcafd8f828debadc771c79c390104a8a92
ClaKaJe <clakajedev@gmail.com>
2024-05-21 20:41:05 +0100

    commit

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
 
-require_once realpath(__DIR__ . '/../vendor/autoload.php');
-
-use Dotenv\Dotenv;
-
-$dotenv = Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-
 include 'components/user_header.php';
 
 include 'components/save_send.php';
